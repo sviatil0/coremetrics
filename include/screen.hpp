cca12ca82a12326203cbf4ceae195f3477ebe5dd
Alicia Melotik <amelotik@student12.cse.nd.edu>
2026-02-18 20:58:47 -0500

    set up screen.hpp with RAII and templated methods

--- include/screen.hpp
+++ include/screen.hpp
@@ -10,17 +10,57 @@ class Screen
 private:
     unsigned int width;
     unsigned int height;
-    SDL_Surface* window;
+    SDL_Surface* surface = nullptr;
 
 public:
-    Screen() = default;
-    Screen(unsigned int w, unsigned int h); //constructors,
+    Screen() = delete; //if no args passed to constructor, cannot create object
+    Screen(unsigned int w, unsigned int h) : width(w), height(h), surface(nullptr)
+    {
+        if (width == 0 || height == 0)
+        {
+            std::cerr << "Dimensions must be >0" << '\n';
+        }
+        surface = SDL_CreateSurface(width, height, SDL_PIXELFORMAT_RGBA8888);
+        if (!surface)
+        {
+            std::cerr << "Failed to create surface: " << SDL_GetError() << '\n';
+        };
+    }
 
-    //always use floats: if user wants to use ints it still works?
-    //otherwise need 3 overloads for all combo of int and float?
-    void drawPixel(const vec2<float>& pos, const vec3<float>& color); 
-    void blitTo(SDL_Surface* toWindow);
-    void drawLine(const vec2<float>& a, const vec2<float>& b); //Bresenham line implementation
-    void drawBox(const vec2<float>& min, const vec2<float>& max); //determine which is min and which is max within method
+    ~Screen() 
+    {
+        if (surface) {
+            SDL_DestroySurface(surface);
+            surface = nullptr;
+        }
+    }
+
+    
+    //need templated functions (below) in order to provide support for both int and float vectors
+    //also means whole implementation must be within header file (like for vec2 and vec3)
+    template<typename T, typename U>
+    void drawPixel(const vec2<T>& pos, const vec3<U>& color)
+    {
+
+    }
+
+    template<typename T, typename U, typename V>
+    void drawLine(const vec2<T>& a, const vec2<U>& b, const vec3<V>& color)
+    {
+        //Bresenham line implementation
+    }
+
+    template<typename T, typename U, typename V>
+    void drawBox(const vec2<T>& a, const vec2<U>& b, const vec3<V>& color)
+    {
+        //determine which vec is min and which is max within method
+    }
+
+    void blitTo(SDL_Surface* target)
+    {
+
+    }
+
 };
+
 #endif
