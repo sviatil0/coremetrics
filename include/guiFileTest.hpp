c4ee9374f99ef1e3250ae18c2db1253ad935650a
Alicia Melotik <amelotik@student12.cse.nd.edu>
2026-02-27 10:42:26 -0500

    fix errors in gui file test

--- include/guiFileTest.hpp
+++ include/guiFileTest.hpp
@@ -1,6 +1,13 @@
 #ifndef __GUIFILE_TEST_HPP__
 #define __GUIFILE_TEST_HPP__
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include "GUIFile.hpp"
+
 void guiFileTestSuite();
 
 #endif
