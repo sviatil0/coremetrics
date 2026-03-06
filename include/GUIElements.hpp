f60949c366ea901f4c62a7726252c48c072d6a7a
Sviatoslav <soleksiienko1@gmail.com>
2026-03-05 19:52:59 -0500

    Added the abtract class and converted the classes for Line Box, and Point to support it

--- include/GUIElements.hpp
+++ include/GUIElements.hpp
@@ -2,22 +2,64 @@
 #define __GUIELEMENTS_HPP__
 #include "vec2.hpp"
 #include "vec3.hpp"
+#include "screen.hpp"
 
-struct Point {
-    vec2 pos;
-    vec3 color;
+enum class GUIElementType
+{
+    POINT,
+    LINE,
+    BOX
+};
+class GUIElement
+{
+public:
+    virtual void draw() = 0;
+    virtual ~GUIElement() = default;
 };
 
-struct Line {
+class Point : public GUIElement
+{
+private:
+    vec2 pos;
+    vec3 color;
+    Screen &screen;
+
+public:
+    Point(vec2 pos, vec3 color, Screen &screen) : pos(pos), color(color), screen(screen)
+    {
+    }
+
+    void draw() override;
+};
+
+class Line : public GUIElement
+{
+private:
     vec2 start;
     vec2 end;
     vec3 color;
+    Screen &screen;
+
+public:
+    Line(vec2 start, vec2 end, vec3 color, Screen &screen) : start(start), end(end), color(color), screen(screen)
+    {
+    }
+    void draw() override;
 };
 
-struct Box {
+class Box : public GUIElement
+{
+private:
     vec2 minPos;
     vec2 maxPos;
     vec3 color;
+    Screen &screen;
+
+public:
+    Box(vec2 minPos, vec2 maxPos, vec3 color, Screen &screen) : minPos(minPos), maxPos(maxPos), color(color), screen(screen)
+    {
+    }
+    void draw() override;
 };
 
 #endif
