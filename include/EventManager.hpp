0e9aa23a13a978a62b3710500511617a5e94de2d
Alicia Melotik <amelotik@student12.cse.nd.edu>
2026-04-04 16:18:31 -0400

    add find layout by name to event manager

--- include/EventManager.hpp
+++ include/EventManager.hpp
@@ -18,13 +18,14 @@ private:
     EventManager& operator=(const EventManager&) = delete;
 
     static bool trickleEvent(Tree<Layout>& node, Event* event, ivec2 parentStart, ivec2 parentEnd);
-    static Tree<Layout>* findLayoutByName(Tree<Layout>& node, const std::string& name);
 
 public:
     static EventManager& getInstance();
 
     void pushEvent(std::unique_ptr<Event> event);
     void processEvents(ivec2 screenStart, ivec2 screenEnd);
+    static Tree<Layout>* findLayoutByName(Tree<Layout>& node, const std::string& name);
+
 };
 
 #endif
