cb12c1d14cfb9c5e369e56df9b80e4b7430e83d7
Sviatoslav <soleksiienko1@gmail.com>
2026-04-21 20:10:12 -0400

    Extend GUIElementFactory and enum for Bar/Row/Label/Button, add Button targetLayoutHide

--- src/GUIElementFactory.cpp
+++ src/GUIElementFactory.cpp
@@ -16,6 +16,38 @@ std::unique_ptr<GUIElement> GUIElementFactory::createBox(vec2 minPos, vec2 maxPo
     return std::make_unique<Box>(minPos, maxPos, color);
 }
 
+std::unique_ptr<GUIElement> GUIElementFactory::createBar(ivec2 minPos, ivec2 maxPos,
+                                                         vec3 fillColor, vec3 bgColor,
+                                                         float minVal, float maxVal,
+                                                         std::string metricName)
+{
+    return std::make_unique<Bar>(minPos, maxPos, fillColor, bgColor, minVal, maxVal, std::move(metricName));
+}
+
+std::unique_ptr<GUIElement> GUIElementFactory::createRow(ivec2 minPos, ivec2 maxPos,
+                                                         std::vector<std::string> cells,
+                                                         std::vector<float> columnWeights,
+                                                         vec3 textColor)
+{
+    return std::make_unique<Row>(minPos, maxPos, std::move(cells), std::move(columnWeights), textColor);
+}
+
+std::unique_ptr<GUIElement> GUIElementFactory::createLabel(std::string text, ivec2 pos, vec3 color)
+{
+    return std::make_unique<Label>(std::move(text), pos, color);
+}
+
+std::unique_ptr<GUIElement> GUIElementFactory::createButton(ivec2 minPos, ivec2 maxPos, vec3 color,
+                                                            std::string soundFile,
+                                                            std::string targetLayout,
+                                                            std::string targetLayoutHide)
+{
+    return std::make_unique<Button>(minPos, maxPos, color,
+                                    std::move(soundFile),
+                                    std::move(targetLayout),
+                                    std::move(targetLayoutHide));
+}
+
 std::unique_ptr<GUIElement> GUIElementFactory::create(GUIElementType type, vec2 pos1, vec2 pos2, vec3 color)
 {
     switch (type)
@@ -26,6 +58,12 @@ std::unique_ptr<GUIElement> GUIElementFactory::create(GUIElementType type, vec2 
         return createLine(pos1, pos2, color);
     case GUIElementType::BOX:
         return createBox(pos1, pos2, color);
+    case GUIElementType::BAR:
+    case GUIElementType::ROW:
+    case GUIElementType::LABEL:
+    case GUIElementType::BUTTON:
+        std::cerr << "GUIElementFactory: type requires specialized overload\n";
+        return nullptr;
     }
     std::cerr << "GUIElementFactory: unknown element type\n";
     return nullptr;
